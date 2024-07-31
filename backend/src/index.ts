8c9aabc3e7247d812f6b0e7e0840e903559249d2
adparab97 <adity@Adityas-MacBook-Air-3.local>
2024-07-31 15:15:15 -0700

    search working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import * as RecipeAPI from "./recipe-api";
+
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
@@ -18,58 +19,11 @@ app.get("/api/recipes/search", async (req, res) => {
   return res.json(results);
 });
 
-app.get("/api/recipes/:recipeId/summary", async (req, res) => {
-  const recipeId = req.params.recipeId;
-  const results = await RecipeAPI.getRecipeSummary(recipeId);
-  return res.json(results);
-});
 
-app.post("/api/recipes/favourite", async (req, res) => {
-  const recipeId = req.body.recipeId;
 
-  try {
-    const favouriteRecipe = await prismaClient.favouriteRecipes.create({
-      data: {
-        recipeId: recipeId,
-      },
-    });
-    return res.status(201).json(favouriteRecipe);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: "Oops, something went wrong" });
-  }
-});
 
-app.get("/api/recipes/favourite", async (req, res) => {
-  try {
-    const recipes = await prismaClient.favouriteRecipes.findMany();
-    const recipeIds = recipes.map((recipe) => recipe.recipeId.toString());
 
-    const favourites = await RecipeAPI.getFavouriteRecipesByIDs(recipeIds);
 
-    return res.json(favourites);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: "Oops, something went wrong" });
-  }
-});
-
-app.delete("/api/recipes/favourite", async (req, res) => {
-  const recipeId = req.body.recipeId;
-
-  try {
-    await prismaClient.favouriteRecipes.delete({
-      where: {
-        recipeId: recipeId,
-      },
-    });
-    return res.status(204).send();
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: "Oops, something went wrong" });
-  }
-});
-
-app.listen(5000, () => {
+app.listen(5100, () => {
   console.log("server running on localhost:5000");
 });
