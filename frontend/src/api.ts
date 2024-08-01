79f3280f00b650cdd586bb75e86603ec62aab195
adparab97 <adity@Adityas-MacBook-Air-3.local>
2024-08-01 00:06:39 -0700

    frontend

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,11 @@
 import { Recipe } from "./types";
 
+
+
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export const searchRecipes = async (searchTerm: string, page: number) => {
-  const baseUrl = new URL("http://localhost:5100/api/recipes/search");
+  const baseUrl = new URL(`${VITE_API_BASE_URL}/api/recipes/search`);
   baseUrl.searchParams.append("searchTerm", searchTerm);
   baseUrl.searchParams.append("page", String(page));
 
@@ -14,7 +18,7 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
 };
 
 export const getRecipeSummary = async (recipeId: string) => {
-  const url = new URL(`http://localhost:5100/api/recipes/${recipeId}/summary`);
+  const url = new URL(`${VITE_API_BASE_URL}/api/recipes/${recipeId}/summary`);
   const response = await fetch(url);
 
   if (!response.ok) {
@@ -25,7 +29,7 @@ export const getRecipeSummary = async (recipeId: string) => {
 };
 
 export const getFavouriteRecipes = async () => {
-  const url = new URL("http://localhost:5100/api/recipes/favourite");
+  const url = new URL(`${VITE_API_BASE_URL}/api/recipes/favourite`);
   const response = await fetch(url);
 
   if (!response.ok) {
@@ -36,7 +40,7 @@ export const getFavouriteRecipes = async () => {
 };
 
 export const addFavouriteRecipe = async (recipe: Recipe) => {
-  const url = new URL("http://localhost:5100/api/recipes/favourite");
+  const url = new URL(`${VITE_API_BASE_URL}/api/recipes/favourite`);
   const body = {
     recipeId: recipe.id,
   };
@@ -55,7 +59,7 @@ export const addFavouriteRecipe = async (recipe: Recipe) => {
 };
 
 export const removeFavouriteRecipe = async (recipe: Recipe) => {
-  const url = new URL("http://localhost:5100/api/recipes/favourite");
+  const url = new URL(`${VITE_API_BASE_URL}/api/recipes/favourite`);
   const body = {
     recipeId: recipe.id,
   };
